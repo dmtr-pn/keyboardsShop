5f098844cf97912289787e1adbfbdb18b74f1b1c
Dmitry <dmutromasteroff@gmail.com>
2023-01-03 18:35:10 +0300

    firts commit

---
+++ client/src/views/Home.vue
@@ -0,0 +1,205 @@
+<template>
+    <Header />
+    <div class="main-name">
+        Top selling
+    </div>
+    <div class="main__ container">
+        <div class="main__img"></div>
+        <div class="main__content">
+            <div class="main__content-name">
+                <div class="main__content-name-name">Varmilo Lavender 108</div>
+                <div class="main__content-name-size">100%</div>
+            </div>
+            <div class="main__content-price">
+                <div class="main__content-price-price">100 $</div>
+                <button class="main__content-price-btn" type="submit">Add to cart</button>
+            </div>
+        </div>
+    </div>
+    <div class="categories__ container">
+        <div class="categories-">Categories</div>
+        <div class="categories__grid">
+            <div class="categories__grid-item">
+                <div>
+                    <div class="categories__grid-item-name">Varmilo</div>
+                    <div class="categories__grid-item-category">Keyboards</div>
+                </div>
+                <div class="categories__grid-item-category-icn">
+                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.33335 39.5833C7.18752 39.5833 6.20697 39.1757 5.39169 38.3604C4.57502 37.5437 4.16669 36.5625 4.16669 35.4166V14.5833C4.16669 13.4375 4.57502 12.4569 5.39169 11.6416C6.20697 10.825 7.18752 10.4166 8.33335 10.4166H41.6667C42.8125 10.4166 43.7938 10.825 44.6104 11.6416C45.4257 12.4569 45.8334 13.4375 45.8334 14.5833V35.4166C45.8334 36.5625 45.4257 37.5437 44.6104 38.3604C43.7938 39.1757 42.8125 39.5833 41.6667 39.5833H8.33335ZM16.6667 33.3333H33.3334V29.1666H16.6667V33.3333ZM10.4167 27.0833H14.5834V22.9166H10.4167V27.0833ZM16.6667 27.0833H20.8334V22.9166H16.6667V27.0833ZM22.9167 27.0833H27.0834V22.9166H22.9167V27.0833ZM29.1667 27.0833H33.3334V22.9166H29.1667V27.0833ZM35.4167 27.0833H39.5834V22.9166H35.4167V27.0833ZM10.4167 20.8333H14.5834V16.6666H10.4167V20.8333ZM16.6667 20.8333H20.8334V16.6666H16.6667V20.8333ZM22.9167 20.8333H27.0834V16.6666H22.9167V20.8333ZM29.1667 20.8333H33.3334V16.6666H29.1667V20.8333ZM35.4167 20.8333H39.5834V16.6666H35.4167V20.8333Z" fill="white"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="categories__grid-item pink">
+                <div>
+                    <div class="categories__grid-item-name">Leopold</div>
+                    <div class="categories__grid-item-category">Keyboards</div>
+                </div>
+                <div class="categories__grid-item-category-icn">
+                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.33335 39.5833C7.18752 39.5833 6.20697 39.1757 5.39169 38.3604C4.57502 37.5437 4.16669 36.5625 4.16669 35.4166V14.5833C4.16669 13.4375 4.57502 12.4569 5.39169 11.6416C6.20697 10.825 7.18752 10.4166 8.33335 10.4166H41.6667C42.8125 10.4166 43.7938 10.825 44.6104 11.6416C45.4257 12.4569 45.8334 13.4375 45.8334 14.5833V35.4166C45.8334 36.5625 45.4257 37.5437 44.6104 38.3604C43.7938 39.1757 42.8125 39.5833 41.6667 39.5833H8.33335ZM16.6667 33.3333H33.3334V29.1666H16.6667V33.3333ZM10.4167 27.0833H14.5834V22.9166H10.4167V27.0833ZM16.6667 27.0833H20.8334V22.9166H16.6667V27.0833ZM22.9167 27.0833H27.0834V22.9166H22.9167V27.0833ZM29.1667 27.0833H33.3334V22.9166H29.1667V27.0833ZM35.4167 27.0833H39.5834V22.9166H35.4167V27.0833ZM10.4167 20.8333H14.5834V16.6666H10.4167V20.8333ZM16.6667 20.8333H20.8334V16.6666H16.6667V20.8333ZM22.9167 20.8333H27.0834V16.6666H22.9167V20.8333ZM29.1667 20.8333H33.3334V16.6666H29.1667V20.8333ZM35.4167 20.8333H39.5834V16.6666H35.4167V20.8333Z" fill="white"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="categories__grid-item blue">
+                <div>
+                    <div class="categories__grid-item-name">Ducky</div>
+                    <div class="categories__grid-item-category">Keyboards</div>
+                </div>
+                <div class="categories__grid-item-category-icn">
+                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.33335 39.5833C7.18752 39.5833 6.20697 39.1757 5.39169 38.3604C4.57502 37.5437 4.16669 36.5625 4.16669 35.4166V14.5833C4.16669 13.4375 4.57502 12.4569 5.39169 11.6416C6.20697 10.825 7.18752 10.4166 8.33335 10.4166H41.6667C42.8125 10.4166 43.7938 10.825 44.6104 11.6416C45.4257 12.4569 45.8334 13.4375 45.8334 14.5833V35.4166C45.8334 36.5625 45.4257 37.5437 44.6104 38.3604C43.7938 39.1757 42.8125 39.5833 41.6667 39.5833H8.33335ZM16.6667 33.3333H33.3334V29.1666H16.6667V33.3333ZM10.4167 27.0833H14.5834V22.9166H10.4167V27.0833ZM16.6667 27.0833H20.8334V22.9166H16.6667V27.0833ZM22.9167 27.0833H27.0834V22.9166H22.9167V27.0833ZM29.1667 27.0833H33.3334V22.9166H29.1667V27.0833ZM35.4167 27.0833H39.5834V22.9166H35.4167V27.0833ZM10.4167 20.8333H14.5834V16.6666H10.4167V20.8333ZM16.6667 20.8333H20.8334V16.6666H16.6667V20.8333ZM22.9167 20.8333H27.0834V16.6666H22.9167V20.8333ZM29.1667 20.8333H33.3334V16.6666H29.1667V20.8333ZM35.4167 20.8333H39.5834V16.6666H35.4167V20.8333Z" fill="white"/>
+                    </svg>
+                </div>
+            </div>
+            <div class="categories__grid-item green">
+                <div>
+                    <div class="categories__grid-item-name">Vortex</div>
+                    <div class="categories__grid-item-category">Keyboards</div>
+                </div>
+                <div class="categories__grid-item-category-icn">
+                    <svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M8.33335 39.5833C7.18752 39.5833 6.20697 39.1757 5.39169 38.3604C4.57502 37.5437 4.16669 36.5625 4.16669 35.4166V14.5833C4.16669 13.4375 4.57502 12.4569 5.39169 11.6416C6.20697 10.825 7.18752 10.4166 8.33335 10.4166H41.6667C42.8125 10.4166 43.7938 10.825 44.6104 11.6416C45.4257 12.4569 45.8334 13.4375 45.8334 14.5833V35.4166C45.8334 36.5625 45.4257 37.5437 44.6104 38.3604C43.7938 39.1757 42.8125 39.5833 41.6667 39.5833H8.33335ZM16.6667 33.3333H33.3334V29.1666H16.6667V33.3333ZM10.4167 27.0833H14.5834V22.9166H10.4167V27.0833ZM16.6667 27.0833H20.8334V22.9166H16.6667V27.0833ZM22.9167 27.0833H27.0834V22.9166H22.9167V27.0833ZM29.1667 27.0833H33.3334V22.9166H29.1667V27.0833ZM35.4167 27.0833H39.5834V22.9166H35.4167V27.0833ZM10.4167 20.8333H14.5834V16.6666H10.4167V20.8333ZM16.6667 20.8333H20.8334V16.6666H16.6667V20.8333ZM22.9167 20.8333H27.0834V16.6666H22.9167V20.8333ZM29.1667 20.8333H33.3334V16.6666H29.1667V20.8333ZM35.4167 20.8333H39.5834V16.6666H35.4167V20.8333Z" fill="white"/>
+                    </svg>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+    name: 'Home',
+    components: {
+        Header
+    }
+}
+</script>
+
+<style scoped>
+.categories- {
+    font-weight: 700;
+    font-size: 32px;
+    margin-bottom: 20px;
+    margin-top: 20px;
+}
+.categories__ {
+    padding: 0 20px;
+}
+.categories__grid {
+    gap: 20px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+.categories__grid-item {
+    padding: 20px;
+    background: linear-gradient(312.91deg, #000000 0%, #353535 100%);
+    border-radius: 25px;
+
+    width: calc(25% - 55px);
+    background-color: #eee;
+}
+.categories__grid-item.pink {
+    background: linear-gradient(312.91deg, #590D65 0%, #A4429A 100%);
+}
+.categories__grid-item.green{
+    background: linear-gradient(312.91deg, #244100 0%, #79A442 100%);
+}
+.categories__grid-item.blue {
+    background: linear-gradient(312.91deg, #00265F 0%, #4269A4 100%);
+}
+
+.categories__grid-item-name {
+    font-weight: 700;
+    font-size: 64px;
+    color: #fff;
+}
+.categories__grid-item-category {
+    font-weight: 400;
+    font-size: 32px;
+    color: #fff;
+}
+.categories__grid-item-category-icn {
+    margin-top: 120px;
+}
+
+.main__ {
+    padding: 0 20px;
+    margin-top: 100px;
+    margin-bottom: 150px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.main-name {
+    text-align: center;
+    margin-top: 20px;
+    font-weight: 700;
+    font-size: 64px;
+}
+.main__img {
+    max-width: 453px;
+    width: 100%;
+    height: 293px;
+
+    background-image: url('../assets/9.jpg');
+    background-size: cover;
+    border-radius: 25px;
+}
+.main__content {
+    margin-left: 20px;
+}
+.main__content-name-name {
+    font-weight: 700;
+    font-size: 48px;
+}
+.main__content-name-size {
+    font-weight: 400;
+    font-size: 32px;
+}
+.main__content-price {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.main__content-price-price {
+    font-weight: 700;
+    font-size: 48px;
+}
+.main__content-price-btn {
+    padding: 10px 25px;
+    background: linear-gradient(99.04deg, #353535 0.88%, #000000 100%);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 25px;
+    color: #FFFFFF;
+    font-weight: 700;
+    font-size: 20px;
+    border: none;
+}
+
+@media screen  and (max-width: 1024px){
+    .categories__grid-item {
+        width: calc(33% - 51px);
+    }
+}
+@media screen  and (max-width: 768px) {
+    .categories__grid-item {
+        width: 100%;
+    }
+    .main__ {
+        padding: 0 20px;
+        margin-top: 20px;
+        display: block;
+        margin-bottom: 0;
+    }
+    .main__content {
+        margin-left: 0;
+    }
+    .main__img {
+        max-width: 100%;
+    }
+}
+</style>
---
+++ client/src/views/Products.vue
@@ -0,0 +1,404 @@
+<template>
+    <div class="dark" v-bind:class="{actv: this.modal}" v-on:click="filter()"></div>
+    <Header />
+    <Transition name="modal">
+        <div v-if="this.modal" class="modal">
+            <div class="modal__inner">
+
+                
+                <div class="mobile-filter__">
+                    <div class="filter__section">
+                        <div class="filter__section-name">Price</div>
+                        <div class="filter__section-bodys">
+                            <select class="filter__section-body-sel" name="currency" id="currency" v-model="filterCurrency">
+                                <option>USD</option>
+                                <option>RUB</option>
+                                <option>EUR</option>
+                            </select>
+
+                            <div class="min-max">
+                                <input type="text" placeholder="Min" class="minm">
+                                <div class="to">to</div>
+                                <input type="text" placeholder="Max" class="minm">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="filter__section">
+                        <div class="filter__section-name">Size</div>
+                        <div class="filter__section-body">
+                            <input type="checkbox" class="hide" id="100" v-model="filterSize" value="100" autocomplete="off" checked/>
+                            <label class="filter__section-body-item" for="100">100%</label>
+
+                            <input type="checkbox" class="hide" id="btn-check" v-model="filterSize" value="90" autocomplete="off" />
+                            <label class="filter__section-body-item" for="btn-check">90%</label>
+
+                            <input type="checkbox" class="hide" id="80" v-model="filterSize" value="80" autocomplete="off" />
+                            <label class="filter__section-body-item" for="80">80%</label>
+
+                            <input type="checkbox" class="hide" id="75" v-model="filterSize" value="75" autocomplete="off" />
+                            <label class="filter__section-body-item" for="75">75%</label>
+
+                            <input type="checkbox" class="hide" id="65" v-model="filterSize" value="65" autocomplete="off" />
+                            <label class="filter__section-body-item" for="65">65%</label>
+
+                            <input type="checkbox" class="hide" id="60" v-model="filterSize" value="60" autocomplete="off" />
+                            <label class="filter__section-body-item" for="60">60%</label>
+                            
+                            <input type="checkbox" class="hide" id="40" v-model="filterSize" value="40" autocomplete="off" />
+                            <label class="filter__section-body-item" for="40">40%</label>
+                            
+                            <input type="checkbox" class="hide" id="20" v-model="filterSize" value="20" autocomplete="off" />
+                            <label class="filter__section-body-item" for="20">20%</label>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="filter__button">Apply</button>
+
+            </div>
+        </div>
+    </Transition>
+    <div class="container" style="margin-top: 20px">
+        <div class="filter__mobile">
+            <button type="submit" class="filter__mobile-btn" v-on:click="filter()">
+                <svg width="33" height="33" viewBox="0 0 33 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M16.5 20.4333C16.3223 20.4333 16.1556 20.4053 16 20.3493C15.8445 20.2942 15.7 20.2 15.5667 20.0667L9.40004 13.9C9.1556 13.6556 9.03915 13.3498 9.05071 12.9827C9.06138 12.6165 9.18893 12.3111 9.43338 12.0667C9.67782 11.8222 9.98893 11.7 10.3667 11.7C10.7445 11.7 11.0556 11.8222 11.3 12.0667L16.5 17.2667L21.7334 12.0333C21.9778 11.7889 22.2836 11.672 22.6507 11.6827C23.0169 11.6942 23.3223 11.8222 23.5667 12.0667C23.8112 12.3111 23.9334 12.6222 23.9334 13C23.9334 13.3778 23.8112 13.6889 23.5667 13.9333L17.4334 20.0667C17.3 20.2 17.1556 20.2942 17 20.3493C16.8445 20.4053 16.6778 20.4333 16.5 20.4333Z" fill="black"/>
+                </svg> Filter</button>
+        </div>
+        <div class="main">
+            <div class="filter__">
+                <div class="filter__section">
+                    <div class="filter__section-name">Size</div>
+                    <div class="filter__section-body">
+                        <input type="checkbox" class="hide" id="100" v-model="filterSize" value="100" autocomplete="off" checked/>
+                        <label class="filter__section-body-item" for="100">100%</label>
+
+                        <input type="checkbox" class="hide" id="btn-check" v-model="filterSize" value="90" autocomplete="off" />
+                        <label class="filter__section-body-item" for="btn-check">90%</label>
+
+                        <input type="checkbox" class="hide" id="80" v-model="filterSize" value="80" autocomplete="off" />
+                        <label class="filter__section-body-item" for="80">80%</label>
+
+                        <input type="checkbox" class="hide" id="75" v-model="filterSize" value="75" autocomplete="off" />
+                        <label class="filter__section-body-item" for="75">75%</label>
+
+                        <input type="checkbox" class="hide" id="65" v-model="filterSize" value="65" autocomplete="off" />
+                        <label class="filter__section-body-item" for="65">65%</label>
+
+                        <input type="checkbox" class="hide" id="60" v-model="filterSize" value="60" autocomplete="off" />
+                        <label class="filter__section-body-item" for="60">60%</label>
+                        
+                        <input type="checkbox" class="hide" id="40" v-model="filterSize" value="40" autocomplete="off" />
+                        <label class="filter__section-body-item" for="40">40%</label>
+                        
+                        <input type="checkbox" class="hide" id="20" v-model="filterSize" value="20" autocomplete="off" />
+                        <label class="filter__section-body-item" for="20">20%</label>
+                    </div>
+                </div>
+
+                <div class="filter__section">
+                    <div class="filter__section-name">Price</div>
+                    <div class="filter__section-bodys">
+                        <select class="filter__section-body-sel" name="currency" id="currency" v-model="filterCurrency">
+                            <option>USD</option>
+                            <option>RUB</option>
+                            <option>EUR</option>
+                        </select>
+
+                        <div class="min-max">
+                            <input type="text" placeholder="Min" class="minm">
+                            <div class="to">to</div>
+                            <input type="text" placeholder="Max" class="minm">
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" class="filter__button">Apply</button>
+            </div>
+            <div class="main__">
+                <div class="main__item">
+                    <div class="main__item-img"></div>
+                    <div class="main__item-info">
+                        <div class="main__item-info-name">Varmilo Panda R2</div>
+                        <div class="main__item-info-size">65%</div>
+                        <div class="main__item-info-footer">
+                            <div class="main__item-info-footer-price">120 $</div>
+                            <button class="main__item-info-footer-button" type="submit">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="main__item">
+                    <div class="main__item-img"></div>
+                    <div class="main__item-info">
+                        <div class="main__item-info-name">Varmilo Panda R2</div>
+                        <div class="main__item-info-size">65%</div>
+                        <div class="main__item-info-footer">
+                            <div class="main__item-info-footer-price">120 $</div>
+                            <button class="main__item-info-footer-button" type="submit">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="main__item">
+                    <div class="main__item-img"></div>
+                    <div class="main__item-info">
+                        <div class="main__item-info-name">Varmilo Panda R2</div>
+                        <div class="main__item-info-size">65%</div>
+                        <div class="main__item-info-footer">
+                            <div class="main__item-info-footer-price">120 $</div>
+                            <button class="main__item-info-footer-button" type="submit">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="main__item">
+                    <div class="main__item-img"></div>
+                    <div class="main__item-info">
+                        <div class="main__item-info-name">Varmilo Panda R2</div>
+                        <div class="main__item-info-size">65%</div>
+                        <div class="main__item-info-footer">
+                            <div class="main__item-info-footer-price">120 $</div>
+                            <button class="main__item-info-footer-button" type="submit">Add to cart</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from '../components/Header.vue'
+
+export default {
+    name: "Products",
+    components: {
+        Header
+    },
+    data() {
+        return{
+            modal: false,
+            filterSize: ["100"],
+            filterCurrency: "USD"
+        }
+    },
+    methods: {
+        filter() {
+            this.modal = !this.modal;
+            if(this.modal){
+                document.getElementById("app").classList.add("blur")
+            }else {
+                document.getElementById("app").classList.remove("blur")
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.filter__section-body-sel {
+    font-weight: 400;
+    font-size: 24px;
+    padding: 10px 20px;
+    border: 2px solid #EAE9E9;
+    border-radius: 10px;
+}
+.filter__section-bodys {
+    display: flex;
+    justify-content: space-between;
+}
+.filter__button {
+    font-weight: 700;
+    font-size: 20px;
+    background: linear-gradient(99.04deg, #353535 0.88%, #000000 100%);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 25px;
+    color: #fff;
+    padding: 10px;
+    width: 100%;
+    margin-top: 10px;
+}
+.min-max {
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
+}
+.to {
+    font-weight: 700;
+    font-size: 24px;
+    margin: 0 10px;
+}
+.minm {
+    width: 45px;
+    font-weight: 400;
+    font-size: 24px;
+    padding: 10px 20px;
+    border: 2px solid #EAE9E9;
+    border-radius: 10px;
+}
+
+.hide {
+    position: absolute;
+    clip: rect(0,0,0,0);
+    pointer-events: none;
+}
+.filter__section-body-item:hover {
+    cursor: pointer;
+}
+.filter__section-body-item {
+    font-weight: 400;
+    font-size: 16px;
+    padding: 10px;
+    border: 2px solid #000000;
+    border-radius: 10px;
+    user-select: none;
+}
+.filter__section-body input:checked + label{
+    background: linear-gradient(113.3deg, #353535 0%, #000000 100%);
+    color: #fff ;
+}
+
+.filter__section-name {
+    font-weight: 600;
+    font-size: 32px;
+}
+.filter__section-body {
+    gap: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    margin-top: 10px;
+    margin-bottom: 20px;
+}
+
+.modal-enter-active, .modal-leave-active {
+	transition: all 0.3s ease-in-out;
+}
+.modal-enter-from,
+.modal-leave-to {
+  transform: translateY(30px);
+  opacity: 0;
+}
+.modal {
+    position: fixed;
+    z-index: 999;
+    bottom: 0px;
+    width: 100%;
+    background: #fff;
+    border-radius: 25px 25px 0 0;
+}
+.modal__inner {
+    padding: 40px 20px;
+}
+.dark {
+    visibility: hidden;
+    position: fixed;
+    height: 100%;
+    width: 100%;
+    background: rgb(0, 0, 0, .5);
+    opacity: 0;
+    transition: opacity .3s;
+    z-index: 99;
+}
+.dark.actv {
+    visibility: visible;
+    opacity: 1;
+}
+
+.filter__mobile {
+    display: none;
+    margin-bottom: 20px;
+}
+.filter__mobile-btn {
+    width: 100%;
+    font-weight: 700;
+    font-size: 24px;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0px;
+    border: 2px solid #000000;
+    border-radius: 10px;
+    background-color: transparent;
+}
+.main {
+    display: flex;
+    flex-direction: row;
+}
+.filter__ {
+    min-width: 335px;
+    margin-right: 20px;
+}
+.main__ {
+    width: 100%;
+    gap: 20px;
+    display: flex;
+    flex-wrap: wrap;
+}
+.main__item {
+    border-radius: 25px;
+    width: calc(33% - 11.21px);
+    background: linear-gradient(146.14deg, #FFFFFF 0%, #F7F7F7 100%);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+}
+.main__item-img {
+    background: url("../assets/Varmilo_Panda_87_rus_10.jpg");
+    background-position: center;
+    background-size: cover;
+    border-radius: 25px 25px 0 0;
+    height: 240px;
+}
+.main__item-info {
+    padding: 20px;
+}
+.main__item-info-name {
+    font-weight: 700;
+    font-size: 28px;
+}
+.main__item-info-size {
+    font-weight: 400;
+    font-size: 24px;    
+}
+.main__item-info-footer {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+}
+.main__item-info-footer-price {
+    font-weight: 700;
+    font-size: 28px;
+}
+.main__item-info-footer-button {
+    padding: 10px 25px;
+    background: linear-gradient(99.04deg, #353535 0.88%, #000000 100%);
+    box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.2);
+    border-radius: 25px;
+    color: #FFFFFF;
+    font-weight: 700;
+    font-size: 20px;
+    border: none;
+}
+@media screen  and (max-width: 1024px){
+    .main__item-info-footer {
+        flex-direction: column;
+        align-items: center;
+    }
+    .main__item-info-footer-button{
+        width: 100%;
+    }
+    .main__item {
+        width: calc(50% - 11.21px);
+    }
+}
+@media screen  and (max-width: 768px) { 
+    .main__item {
+        width: 100%;
+    }
+    .filter__ {
+        display: none;
+    }
+    .filter__mobile {
+        display: block;
+    }
+}
+</style>
